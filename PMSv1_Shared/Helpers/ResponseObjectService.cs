df52b8f8a2edd1d091c8ceeb13dd43c322626b9a
B0bUy <153715633+B0bUy@users.noreply.github.com>
2025-02-01 18:24:50 +0800

    Added Helpers Folder for Data Manipulation (Parse, etc.)

---
+++ PMSv1_Shared/Helpers/ResponseObjectService.cs
@@ -0,0 +1,14 @@
+﻿using PMSv1_Shared.Entities.Contracts;
+
+namespace PMSv1_Shared.Helpers
+{
+    public class ResponseObjectService
+    {
+        public async Task<T> GetData<T>(HttpResponseMessage response)
+        {
+            var result = await response.Content.ReadAsStringAsync();
+            var data = System.Text.Json.JsonSerializer.Deserialize<ApiResponse<T>>(result);
+            return data == null ? Activator.CreateInstance<T>() : data.Result;
+        }
+    }
+}
