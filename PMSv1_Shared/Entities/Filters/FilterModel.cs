8add2bd0c574187b7b748dcbe6dae0bf6ed5a1bd
B0bUy <153715633+B0bUy@users.noreply.github.com>
2025-02-06 20:40:36 +0800

    Added Filter Class

---
+++ PMSv1_Shared/Entities/Filters/FilterModel.cs
@@ -0,0 +1,34 @@
+﻿namespace PMSv1_Shared.Entities.Filters.FilterModel
+{
+    public enum ProjectFilter
+    {
+        CreatedAt,
+        Deadline,
+        Status,
+        Department,
+        Package,
+        Name
+    }
+    public enum PackageFilter
+    {
+        CreatedAt,
+        Projects,
+        Name,
+    }
+    public enum TaskFilter
+    {
+        CreatedAt,
+        Deadline,
+        Status,
+        Department,
+        Project,
+        Package,
+        Name
+    }
+
+    public class FilterRequest
+    {
+        public int FilterType { get; set; }
+        public string FilterValue { get; set; } = string.Empty;
+    }
+}
